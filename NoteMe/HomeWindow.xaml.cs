936ee3cb151132d9b4f1ff7efab298f7ca84e394
Sw0ne <wandzik.sandra@gmail.com>
2022-03-15 18:04:35 +0100

    Add project files.

---
+++ NoteMe/HomeWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NoteMe
+{
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+
+            // Datum und Wochentag Anzeige
+            var time = DateTime.Today;
+            var weekday = time.DayOfWeek;
+            DatumBox.Text = weekday + ", " + time.ToShortDateString();
+
+            // Kalender-Datum Default = Heute
+            Calendar.SelectedDate = DateTime.Today;
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonZurueck_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonVor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSpeichern_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonLoeschen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NoteMe/WelcomePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NoteMe
+{
+    /// <summary>
+    /// Interaction logic for WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Page
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AnmeldeButton_Click(object sender, RoutedEventArgs e)
+        {
+            HomeWindow homeWindow = new HomeWindow();
+            homeWindow.Show();
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
